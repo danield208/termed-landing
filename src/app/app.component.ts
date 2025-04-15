f6a651ff5797158851ad89cc2fb7dcc28582bc1e
Daniel <dani10513@icloud.com>
2025-04-15 19:45:30 +0200

    basic ionic setup, global header + menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonHeader,
+  IonToolbar,
+  IonMenuButton,
+  IonContent,
+  IonMenu,
+  IonButtons,
+  IonImg,
+  IonNav,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [
+    IonNav,
+    IonImg,
+    IonApp,
+    IonRouterOutlet,
+    IonHeader,
+    IonToolbar,
+    IonMenuButton,
+    IonContent,
+    IonMenu,
+    IonButtons,
+  ],
+  styleUrl: 'app.component.scss',
 })
 export class AppComponent {
   constructor() {}
